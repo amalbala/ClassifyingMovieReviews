099b9ca47813e3f485d730bb335dc7add444cf37
AntonioMartinez <amalbala@gmail.com>
2019-06-07 20:50:56 +0200

    Add new project multi labeling

---
+++ ClassifyNews.py
@@ -0,0 +1,70 @@
+from keras.datasets import reuters
+from keras.utils.np_utils import to_categorical
+from keras import models
+from keras import layers
+import numpy as np
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib import pyplot as plt
+
+(train_data, train_labels), (test_data, test_labels) = reuters.load_data(num_words=10000)
+
+
+def vectorize_sequences(sequences, dimension=10000):
+    result = np.zeros((len(sequences), dimension))
+    for i, sequences in enumerate(sequences):
+        result[i, sequences]=1.
+    return result
+
+
+# transform sequences into tensors
+x_train = vectorize_sequences(train_data)
+x_test = vectorize_sequences(test_data)
+one_hot_train_labels = to_categorical(train_labels)
+one_hot_test_labels = to_categorical(test_labels)
+
+x_val = x_train[:1000]
+partial_x_train = x_train[1000:]
+y_val = one_hot_train_labels[:1000]
+partial_y_train = one_hot_train_labels[1000:]
+
+model = models.Sequential()
+model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(46, activation='softmax'))
+
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['acc'])
+
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=20,
+                    batch_size=128,
+                    validation_data=(x_val, y_val))
+
+history_dict = history.history
+loss_values = history_dict['loss']
+val_loss_values = history_dict['val_loss']
+acc = history_dict['acc']
+val_acc = history_dict['val_acc']
+
+
+epochs = range(1, len(acc) + 1)
+plt.plot(epochs, loss_values, 'bo', label='Training loss')
+plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
+plt.title('Training and Validation Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+plt.clf()
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and Validation acc')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+result = model.evaluate(x_test, one_hot_test_labels)
+predictions = model.predict(x_test)
